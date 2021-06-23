ba2a57a4fbfeb266a714c2ac0f3531a31d2bb0c6
mukeshrock7897 <mukeshrock7897@gmail.com>
2021-06-23 14:02:10 +0530

    entity form data extraction done

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,59 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List 
+
+from rasa_sdk import Action, Tracker 
+from rasa_sdk.events import SlotSet, EventType 
+from rasa_sdk.executor import CollectingDispatcher
+import webbrowser
+from rasa_sdk.types import DomainDict
+
+class ActionVideo(Action):
+    def name(self) -> Text:
+        return "action_video"
+
+    async def run(
+        self,
+        dispatcher,
+        tracker: Tracker,
+        domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        video_url="https://youtu.be/hzTg4zPBtDU"
+        dispatcher.utter_message("wait... Playing your video.")
+        webbrowser.open(video_url)
+        return []
+
+class ValidateRestaurantForm(Action):
+    def name(self) -> Text:
+        return "user_details_form"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        required_slots = ["name", "email" , "profess" , "place"]
+
+        for slot_name in required_slots:
+            if tracker.slots.get(slot_name) is None:
+                # The slot is not filled yet. Request the user to fill this slot next.
+                return [SlotSet("requested_slot", slot_name)]
+
+        # All slots are filled.
+        return [SlotSet("requested_slot", None)]
+
+class ActionSubmit(Action):
+    def name(self) -> Text:
+        return "action_submit"
+
+    def run(
+        self,
+        dispatcher,
+        tracker: Tracker,
+        domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_details_thanks",
+                                 Name=tracker.get_slot("name"),
+                                 Email=tracker.get_slot("email"),
+                                 Profession=tracker.get_slot("profess"),
+                                 Place=tracker.get_slot("place")
+                                 )
